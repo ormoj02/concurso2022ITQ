9f8f99d358ac9058707df2e9db35ad9c0ee5d33b
ormoj02 <ormoj02@gmail.com>
2022-11-24 07:55:40 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace concurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
